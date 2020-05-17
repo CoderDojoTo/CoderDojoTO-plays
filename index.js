59b5e2d437a7513cf7d7da3b2ff277f42a4860ab
DewaldoDev <dewaldodev@gmail.com>
2020-05-17 12:34:29 -0400

    Add the !dice command

--- index.js
+++ index.js
@@ -16,29 +16,43 @@ client.connect();
 client.on("message", (channel, tags, message, self) => {
   if (self) return;
   if (message.toLowerCase() === "!followers") {
-    const coderDojoTOID = 501645429;
-    client.api(
-      {
-        url: `https://api.twitch.tv/helix/users/follows?to_id=${coderDojoTOID}`,
-        method: "GET",
-        headers: {
-          Accept: "application/vnd.twitchtv.v5+json",
-          Authorization: `OAuth ${process.env.TWITCH_OAUTH_TOKEN}`,
-          "Client-ID": process.env.TWITCH_CLIENT_ID,
-        },
-      },
-      (err, res, body) => {
-        const user = body.data.find((userData) => {
-          return userData["from_name"] === tags.username;
-        });
-
-        if (user) {
-          client.say(
-            channel,
-            `${tags.username} have been following since ${user["followed_at"]}`
-          );
-        }
-      }
-    );
+    followersCommand(channel, tags);
+  } else if (message.toLowerCase() === "!dice") {
+    diceCommand(channel, tags);
   }
 });
+
+const followersCommand = (channel) => {
+  const coderDojoTOID = 501645429;
+  client.api(
+    {
+      url: `https://api.twitch.tv/helix/users/follows?to_id=${coderDojoTOID}`,
+      method: "GET",
+      headers: {
+        Accept: "application/vnd.twitchtv.v5+json",
+        Authorization: `OAuth ${process.env.TWITCH_OAUTH_TOKEN}`,
+        "Client-ID": process.env.TWITCH_CLIENT_ID,
+      },
+    },
+    (err, res, body) => {
+      const user = body.data.find((userData) => {
+        return userData["from_name"] === tags.username;
+      });
+
+      if (user) {
+        client.say(
+          channel,
+          `${tags.username} have been following since ${user["followed_at"]}`
+        );
+      } else {
+        client.say("You haven't followed this account yet. You should follow!");
+      }
+    }
+  );
+};
+
+const diceCommand = (channel, tags) => {
+  const roll = Math.floor(Math.random() * 4000 + 1);
+
+  client.say(channel, `${tags.username} rolled a ${roll}`);
+};
